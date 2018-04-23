334664a09284c61d5040e31a46a0b50152548cbb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WpfApplication/MainWindow.xaml.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+
+Namespace WpfApplication
+	''' <summary>
+	''' Interaction logic for MainWindow.xaml
+	''' </summary>
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+	End Class
+End Namespace
