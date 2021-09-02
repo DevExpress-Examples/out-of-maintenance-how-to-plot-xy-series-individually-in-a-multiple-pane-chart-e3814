fc1f0aad4ae525e6e9d1905996c571f1546faa8d
DevExpressExampleBot <fakeEmail>
2021-09-02 20:24:59 +0300

    Source auto update [skip ci]

--- VB/WpfApplication/MainWindow.xaml.vb
+++ VB/WpfApplication/MainWindow.xaml.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System.Windows
+﻿Imports System.Windows
 
 Namespace WpfApplication
 	''' <summary>
@@ -7,6 +6,7 @@ Namespace WpfApplication
 	''' </summary>
 	Partial Public Class MainWindow
 		Inherits Window
+
 		Public Sub New()
 			InitializeComponent()
 		End Sub
